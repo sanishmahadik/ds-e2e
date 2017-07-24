05f7a59f1a9dbff5b6cec0a959b2b0ad67b5ed1f
Sanish Mahadik <smahadik@splunk.com>
2017-07-24 10:47:37 -0700

    sse-c checks

--- object-store-success/object-store-success.sh
+++ object-store-success/object-store-success.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "Running object-store-success scenario"
+echo "Setting signature version to v4"
+aws configure set s3.signature_version s3v4
 
 # hard-code the bucket-name for now
 BUCKET_NAME=sanish-avanti-test
 
 UPLOAD_FILE=upload.txt
 DOWNLOAD_FILE=download.txt
+SSEC_KEY=abcdef1234567890abcdef1234567890
 
 function should_fail() {
 echo "testing for failure: $1"
@@ -42,10 +45,18 @@ case ${TESTNAME} in
   "encrypted-success") 
     # sse specified for upload
     should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse"
-    # good sse algorithm specified
+    # sse-s3 specified explicitly
     should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse AES256"
+    # sse-kms algorithm specified
+    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse aws:kms"
     # download file
     should_succeed "aws s3 cp s3://${BUCKET_NAME}/${UPLOAD_FILE} ${DOWNLOAD_FILE}"
+    # content should match
+    should_succeed "diff ${UPLOAD_FILE} ${DOWNLOAD_FILE}"
+    # sse-c algorithm specified
+    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse-c AES256 --sse-c-key ${SSEC_KEY}"
+    # download file
+    should_succeed "aws s3 cp s3://${BUCKET_NAME}/${UPLOAD_FILE} ${DOWNLOAD_FILE} --sse-c AES256 --sse-c-key ${SSEC_KEY}"
     # content should match
     should_succeed "diff ${UPLOAD_FILE} ${DOWNLOAD_FILE}"
     ;;
