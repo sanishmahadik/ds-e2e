055646d3e32cb346fd28ff770e23adfc8543352c
Sanish Mahadik <smahadik@splunk.com>
2017-07-24 17:00:11 -0700

    remove encryption tests

--- object-store-success/object-store-success.sh
+++ object-store-success/object-store-success.sh
@@ -6,10 +6,10 @@ aws configure set s3.signature_version s3v4
 
 # hard-code the bucket-name for now
 BUCKET_NAME=sanish-avanti-test
+TESTNAME="dummy"
 
 UPLOAD_FILE=upload.txt
 DOWNLOAD_FILE=download.txt
-SSEC_KEY=abcdef1234567890abcdef1234567890
 
 function should_fail() {
 echo "testing for failure: $1"
@@ -36,33 +36,20 @@ return 0
 echo "This is a test file" > ${UPLOAD_FILE}
 
 case ${TESTNAME} in
-  "unencrypted-failure") 
-    # no sse specified for upload
-    should_fail "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE}"
-    # bad sse algorithm specified for upload
-    should_fail "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse AES128"
+  "dummy")
+    aws s3 ls
     ;;
-  "encrypted-success") 
-    # sse specified for upload
-    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse"
-    # sse-s3 specified explicitly
-    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse AES256"
-    # sse-kms algorithm specified
-    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse aws:kms"
+  "check-upload-download")
+    # upload file
+    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE}"
     # download file
     should_succeed "aws s3 cp s3://${BUCKET_NAME}/${UPLOAD_FILE} ${DOWNLOAD_FILE}"
-    # content should match
-    should_succeed "diff ${UPLOAD_FILE} ${DOWNLOAD_FILE}"
-    # sse-c algorithm specified
-    should_succeed "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse-c AES256 --sse-c-key ${SSEC_KEY}"
-    # download file
-    should_succeed "aws s3 cp s3://${BUCKET_NAME}/${UPLOAD_FILE} ${DOWNLOAD_FILE} --sse-c AES256 --sse-c-key ${SSEC_KEY}"
     # content should match
     should_succeed "diff ${UPLOAD_FILE} ${DOWNLOAD_FILE}"
     ;;
   "bad-perms-failure")
     # not able to upload even with valid command
-    should_fail "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE} --sse"
+    should_fail "aws s3 cp ${UPLOAD_FILE} s3://${BUCKET_NAME}/${UPLOAD_FILE}"
     # not able to download
     should_fail "aws s3 cp s3://${BUCKET_NAME}/${UPLOAD_FILE} ${DOWNLOAD_FILE}"
 esac
