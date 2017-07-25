d1a4953f09d2eef597a90fb994589b36ddc9583a
Sanish Mahadik <smahadik@splunk.com>
2017-07-25 14:59:35 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+set -e
+
+PASS_DIR=ds-success
+FAIL_DIR=ds-failure
+PREFIX=sanishmahadik
+PASS_BUILD=${PREFIX}/${PASS_DIR}:latest
+FAIL_BUILD=${PREFIX}/${FAIL_DIR}:latest
+
+echo "************* Building ${PASS_BUILD} *****************"
+cd $PASS_DIR 
+docker build -t $PASS_BUILD . 
+docker run -t -i --env-file env.list  $PASS_BUILD 
+docker push ${PASS_BUILD}
+cd -
+
+echo "************* Building ${FAIL_BUILD} *****************"
+cd $FAIL_DIR
+docker build -t $FAIL_BUILD . 
+docker run -t -i --env-file env.list  $FAIL_BUILD 
+docker push ${FAIL_BUILD}
+cd -
